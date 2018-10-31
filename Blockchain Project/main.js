5807980162752247faf3b0fbac15564e18a47e8d
Aruna Liyanaarachchi <liyanaarachchi@Arunas-MacBook-Pro.local>
2018-10-31 13:14:57 -0400

    Install express

--- Blockchain Project/main.js
+++ Blockchain Project/main.js
@@ -1,4 +1,4 @@
-var Chain = require("./Blockchain.js");
+/*var Chain = require("./Blockchain.js");
 var Block = require("./Block.js");
 //var Person = require("./Person.js");
 var twonCoin = new Chain();
@@ -30,8 +30,55 @@ twonCoin.chain[1].data =  100;
 twonCoin.chain[1].hash = twonCoin.chain[1].calcHash();
 //                                Test Cases
  ////////////////////////////////////////////////////////////////////////////////////// 
-console.log('Is blockChain Valid: ' + twonCoin.isChainValid());                        */
+console.log('Is blockChain Valid: ' + twonCoin.isChainValid());                        
 
 //console.log(root);
 //console.log(JSON.stringify(root2));
-console.log(JSON.stringify(twonCoin, null, 4)); 
+console.log(JSON.stringify(twonCoin, null, 4)); */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const VoteNode = require('./VoteNode'); 
+const VoteChain = require('./VoteChain');
+
+const port = 18071+Math.floor(Math.random()*30);
+console.log('starting node on ', port)
+let node1 = new VoteNode(port);
+let chain = new VoteChain();
+
+node1.init();
+
+const http_port = 3000+Math.floor(Math.random()*10);
+
+let VoteHTTP = function (){
+	const app = new express();
+
+	app.use(bodyParser.json());
+
+	app.get('/addNode/:port', (req, res)=>{
+		console.log('add host: '+req.params.port)
+		node1.addPeer('localhost', req.params.port)
+
+		res.send();
+	})
+
+	app.get('/spawnVote/:teammember', (req, res)=>{
+		let newBlock = node1.createBlock(req.params.teammember);
+		console.log('block created' + newBlock );
+		res.send();
+	})
+
+	app.listen(http_port, () => {
+		console.log(`http server up.. ${http_port}`);
+	})
+
+
+	app.get('/getCurrentChain', (req, res)=>{
+		console.log('Current Chain: ' + JSON.stringify(chain.getChain()) ) ;
+		
+
+		
+	})
+}
+
+let httpserver = new VoteHTTP();
